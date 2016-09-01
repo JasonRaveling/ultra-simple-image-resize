3babeaf8a66dc65cb698f727da61787cf8ab10f3
webunraveling <jason@webunraveling.com>
2016-09-01 15:45:35 -0500

    Adding licensing as prescribed by GPLv3

--- resize.php
+++ resize.php
@@ -1,4 +1,23 @@
 <?php
+/*
+ * Ultra Simple Image Resize - Provides a simple form for people to upload
+ * and then receive a resize image.
+ * Copyright (C) 2016 Jason Raveling
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 // Set target directory to temporarily save images
 $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
